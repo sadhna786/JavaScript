d95590c17177e8a68c6acb8ff85c38601f219a60
sadhna786 <sadhna10jan2001@gmail.com>
2024-06-29 20:19:54 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1221
+let accountEmail = "sadhna@google.com"
+var accountPassword = "12345"
+accountCity = "Kanpur"
+let accountState;
+//accountId = 23  // not allowed
+
+//console.log(accountId);
+
+accountEmail = "abc@gmail.com"
+accountPassword = "345"
+accountCity = "Bengaluru"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version
+
+//alert(3 +3) // we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Sadhna")
+
+
+let name = "abc"
+let id =101
+let isEqual = true
+let state;
+
+
+// primitive data types :-
+
+//number => limit => 2 to power 53
+//bitint
+// string => ""
+//boolean => true / false
+//null => standalone value or null is a object
+//undefined => not assign any value
+// symbol => unique
+
+//object
+
+console.log(typeof name)
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
