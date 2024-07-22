ae291042b924153f827d0465674b75a7d08fb719
sadhna786 <sadhna10jan2001@gmail.com>
2024-07-22 22:00:51 +0530

    stack and heap memory concept

--- 01_basics/dataTypes_Summary.js
+++ 01_basics/dataTypes_Summary.js
@@ -1,37 +1,61 @@
 // Primitive
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false;
-const temp = null
-const email = undefined 
+// const isLoggedIn = false;
+// const temp = null
+// const email = undefined 
 
-console.log(typeof(temp));
-console.log(typeof(email));
+// console.log(typeof(temp));
+// console.log(typeof(email));
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
-const bigNumber = 4455667777665n
-console.log(typeof(bigNumber));
+// const bigNumber = 4455667777665n
+// console.log(typeof(bigNumber));
 
 
 // Reference Type (Non premitive)
 
 //Array, Objects, functions
 
-const heros = ["Shaktiman" , "doreamon" , "Bheem"];
-let myObj = {
-    name : "Nobita",
-    age : 15,
+// const heros = ["Shaktiman" , "doreamon" , "Bheem"];
+// let myObj = {
+//     name : "Nobita",
+//     age : 15,
+// }
+
+// const myFun = function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof(myFun));
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+//  Stack  (premitive), Heap (Non-Premitive)
+
+// let channel = "myYoutubeName"
+// let anotherName = channel
+// anotherName = "favourite"
+
+// console.log(anotherName);
+// console.log(channel);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
 }
 
-const myFun = function(){
-    console.log("hello world");
-}
+let userTwo = userOne
 
-console.log(typeof(myFun));
+userTwo.email = "abc@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
